f56b607bb935750a1ee26b96cf841cbd6491fe53
Rob Hoelz <rob@hoelz.ro>
2012-01-29 10:49:57 -0600

    Add some documentation

--- lib/Text/DokuWiki.pm
+++ lib/Text/DokuWiki.pm
@@ -13,10 +13,30 @@ __END__
 
 =head1 SYNOPSIS
 
+  use Text::DokuWiki;
+
+  my $doku = Text::DokuWiki->new;
+  my $doc  = $doku->parse($text);
+  say $doc->as_html;
+
 =head1 DESCRIPTION
 
-=head1 FUNCTIONS
+This module is a parser for the markup language that
+DokuWiki uses.
+
+=head1 METHODS
+
+=head2 Text::DokuWiki->new
+
+Creates a new DokuWiki syntax parser.
+
+=head2 $doku->parse($text)
+
+Parses C<$text> as a DokuWiki document and returns
+a L<Text::DokuWiki::Document> object.
 
 =head1 SEE ALSO
+
+L<http://dokuwiki.org>
 
 =cut

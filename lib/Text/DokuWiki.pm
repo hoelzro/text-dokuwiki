c48f46e3118b3a1eb844dd2f6b03e1c9441d9af5
Rob Hoelz <rob@hoelz.ro>
2013-01-19 11:29:00 +0100

    Start the transition to a more flexible design

--- lib/Text/DokuWiki.pm
+++ lib/Text/DokuWiki.pm
@@ -44,60 +44,93 @@ my %PSEUDO_HTML_NODE_CLASSES = (
     super => SuperscriptElement,
 );
 
-sub parse {
-    my ( $class, $text ) = @_;
+has current_node => (
+    is      => 'rw',
+    handles => {
+        _append_content => '_append_content',
+    },
+);
 
-    my $doc          = Text::DokuWiki::Document->new;
-    my $current_node = ParagraphElement->new(
-        parent => $doc,
-    );
-    $doc->append_child($current_node);
+sub _append_child {
+    my ( $self, $child, %params ) = @_;
 
-    # XXX this could be cleaner
-    my $pop_text_node = sub {
-        # XXX this logic is teh suck
-        # XXX definitely don't use ref(...) eq $class
-        while(ref($current_node) eq TextElement) {
-            $current_node = $current_node->parent;
-        }
-    };
-
-    my $add_and_replace = sub {
-        my ( $node_class ) = @_;
-
-        $pop_text_node->();
-
-        my $child_node = $node_class->new(
-            parent => $current_node,
+    unless(ref $child) {
+        $child = $child->new(
+            %params,
+            parent => $self->current_node,
         );
-        $current_node->append_child($child_node);
-        $current_node = $child_node;
-    };
+    }
 
-    my $add_pseudo_html_node = sub {
-        my ( $tag_name ) = @_;
+    $self->current_node->append_child($child);
+    return $child;
+}
 
-        my $node_class = $PSEUDO_HTML_NODE_CLASSES{$tag_name};
-        # XXX barf if !$node_class
-        $add_and_replace->($node_class);
-    };
+sub _pop_text_node {
+    my ( $self ) = @_;
 
-    my $remove_pseudo_html_node = sub {
-        my ( $tag_name ) = @_;
-        my $node_class = $PSEUDO_HTML_NODE_CLASSES{$tag_name};
-        # XXX barf if !$node_class
+    my $node = $self->current_node;
 
-        # XXX it should probably be the direct parent, right?
-        while(!$current_node->isa($node_class) &&
-              !$current_node->isa('Text::DokuWiki::Document')) {
+    # XXX this logic is teh suck
+    while($node->isa(TextElement)) {
+        $node = $node->parent;
+    }
+    $self->current_node($node);
+}
 
-            $current_node = $current_node->parent;
-        }
-        if($current_node->isa('Text::DokuWiki::Document')) {
-            # XXX we can't find the matching node, barf
-        }
+sub _down {
+    my ( $self, $child, %params ) = @_;
+
+    $self->_pop_text_node;
+    $child = $self->_append_child($child, %params);
+    $self->current_node($child);
+}
+
+sub _up {
+    my ( $self ) = @_;
+
+    $self->current_node($self->current_node->parent);
+}
+
+# XXX should <del>...</del> create an HTMLElement->new(tag => 'del'), or a
+#     DeletedElement?
+sub _add_pseudo_html_node {
+    my ( $self, $tag_name ) = @_;
+
+    my $node_class = $PSEUDO_HTML_NODE_CLASSES{$tag_name};
+    # XXX barf if !$node_class
+    $self->_down($node_class);
+}
+
+sub _remove_pseudo_html_node {
+    my ( $self, $tag_name ) = @_;
+
+    my $current_node = $self->current_node;
+
+    my $node_class = $PSEUDO_HTML_NODE_CLASSES{$tag_name};
+    # XXX barf if !$node_class
+
+    # XXX it should probably be the direct parent, right?
+    while(!$current_node->isa($node_class) &&
+          !$current_node->isa('Text::DokuWiki::Document')) {
+
         $current_node = $current_node->parent;
-    };
+    }
+    if($current_node->isa('Text::DokuWiki::Document')) {
+        # XXX we can't find the matching node, barf
+    }
+    $self->current_node($current_node->parent);
+}
+
+sub parse {
+    my ( $self, $text ) = @_;
+
+    unless(ref($self)) {
+        $self = $self->new;
+    }
+
+    my $doc = Text::DokuWiki::Document->new;
+    $self->current_node($doc);
+    $self->_down(ParagraphElement);
 
     # XXX getting rid of these would be nice
     my $in_bold_section;
@@ -105,75 +138,70 @@ sub parse {
     my $in_underlined_section;
     my $in_monospace_section;
 
-    # XXX making the syntax "pluggable" would be nice
-
     while($text) {
         my $found_match = 1;
 
         # XXX use \G?
         # XXX compile each component regex into a single regex?
         if($text =~ /\A$HEADER_RE/p) {
-            $pop_text_node->();
-            $current_node->append_child(HeaderElement->new(
+            $self->_pop_text_node;
+            $self->_append_child(HeaderElement,
                 content => $+{'header_content'},
-                parent  => $current_node,
-            ));
+            );
         } elsif($text =~ /\A[*][*]/p) {
             $in_bold_section = !$in_bold_section;
 
             if($in_bold_section) {
-                $add_and_replace->(BoldElement);
+                $self->_down(BoldElement);
             } else {
-                $current_node = $current_node->parent;
+                $self->_up;
             }
         } elsif($text =~ m{\A//}p) {
             $in_italic_section = !$in_italic_section;
 
             if($in_italic_section) {
-                $add_and_replace->(ItalicElement);
+                $self->_down(ItalicElement);
             } else {
-                $current_node = $current_node->parent;
+                $self->_up;
             }
         } elsif($text =~ /\A__/p) {
             $in_underlined_section = !$in_underlined_section;
 
             if($in_underlined_section) {
-                $add_and_replace->(UnderlinedElement);
+                $self->_down(UnderlinedElement);
             } else {
-                $current_node = $current_node->parent;
+                $self->_up;
             }
         } elsif($text =~ /\A''/p) {
             $in_monospace_section = !$in_monospace_section;
 
             if($in_monospace_section) {
-                $add_and_replace->(MonospaceElement);
+                $self->_down(MonospaceElement);
             } else {
-                $current_node = $current_node->parent;
+                $self->_up;
             }
         } elsif($text =~ /\A\n\n/p) {
-            while(ref($current_node) ne 'Text::DokuWiki::Document' &&
-                  ref($current_node) ne ParagraphElement) {
+            while(!$self->current_node->isa('Text::DokuWiki::Document') &&
+                  !$self->current_node->isa(ParagraphElement)) {
 
-                $current_node = $current_node->parent;
+                $self->_up;
             }
-            if($current_node->isa(ParagraphElement)) {
-                $current_node = $current_node->parent;
+            if($self->current_node->isa(ParagraphElement)) {
+                $self->_up;
             }
-            $add_and_replace->(ParagraphElement);
+            $self->_down(ParagraphElement);
         } elsif($text =~ m{\A\\\\[\s\n]}p) {
-            $pop_text_node->();
-            $current_node->append_child(ForcedNewlineElement->new(
-                parent => $current_node,
-            ));
+            $self->_pop_text_node;
+            $self->_append_child(ForcedNewlineElement);
         } elsif($text =~ /\A$OPEN_PSEUDO_HTML_RE/p) {
-            $add_pseudo_html_node->($+{'tag_name'});
+            $self->_add_pseudo_html_node($+{'tag_name'});
         } elsif($text =~ /\A$CLOSE_PSEUDO_HTML_RE/p) {
-            $remove_pseudo_html_node->($+{'tag_name'});
+            $self->_remove_pseudo_html_node($+{'tag_name'});
         } elsif($text =~ /\A./sp) {
-            unless($current_node->_is_textual) {
-                $add_and_replace->(TextElement);
+            unless($self->current_node->_is_textual) {
+                $self->_down(TextElement);
             }
-            $current_node->_append_content(${^MATCH});
+            $self->_append_content(${^MATCH});
         } else {
             $found_match = 0;
         }

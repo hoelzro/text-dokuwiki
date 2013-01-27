45bfe98125be30fa4e8420364984fbfc401902ce
Rob Hoelz <rob@hoelz.ro>
2013-01-27 16:12:47 +0100

    Add footnote element class

---
+++ lib/Text/DokuWiki/Element/Footnote.pm
@@ -0,0 +1,21 @@
+package Text::DokuWiki::Element::Footnote;
+
+use Moose;
+
+extends 'Text::DokuWiki::Element';
+
+sub _is_textual {
+    return 0;
+}
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=cut

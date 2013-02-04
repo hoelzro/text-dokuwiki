52c07b044b920c7498cc489d864bcc18345197bd
Rob Hoelz <rob@hoelz.ro>
2013-02-04 21:45:52 +0100

    Add early versions of List/ListItem elements

---
+++ lib/Text/DokuWiki/Element/List.pm
@@ -0,0 +1,22 @@
+package Text::DokuWiki::Element::List;
+
+use Moose;
+
+extends 'Text::DokuWiki::Element';
+
+has ordered => (
+    is      => 'ro',
+    default => 0,
+);
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=cut
---
+++ lib/Text/DokuWiki/Element/ListItem.pm
@@ -0,0 +1,21 @@
+package Text::DokuWiki::Element::ListItem;
+
+use Moose;
+
+extends 'Text::DokuWiki::Element';
+
+has _indent => (
+    is => 'ro',
+);
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=cut

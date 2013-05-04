160d5bfb3851a123d9619ad73cfade2d7b00ef00
Rob Hoelz <rob@hoelz.ro>
2013-05-04 13:11:19 +0200

    Add missing code element class

---
+++ lib/Text/DokuWiki/Element/Code.pm
@@ -0,0 +1,19 @@
+package Text::DokuWiki::Element::Code;
+
+use Moose;
+
+extends 'Text::DokuWiki::Element';
+
+1;
+
+__END__
+
+# ABSTRACT: Description for Text::DokuWiki::Element::Code
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=cut
